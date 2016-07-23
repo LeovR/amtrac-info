04970ca08a6f6ff374d8f94445d659f97feeb304
Jan Marten <jan.marten@gmx.de>
2016-07-23 13:36:48 +0200

    Add class which sends song signatures

---
+++ SongInfoSceneSignaturePublisher.py
@@ -0,0 +1,19 @@
+from _Framework.ControlSurfaceComponent import ControlSurfaceComponent
+
+
+class SongInfoSceneSignaturePublisher(ControlSurfaceComponent):
+    __module__ = __name__
+    __doc__ = " SongInfoSceneSignaturePublisher "
+
+    def __init__(self, parent, scene):
+        ControlSurfaceComponent.__init__(self)
+        self._parent = parent
+        self._scene = scene
+        scene.add_is_triggered_listener(self.is_triggered_fired)
+
+    def is_triggered_fired(self):
+        if not self._scene.is_triggered:
+            numerator = self.song().signature_numerator
+            denominator = self.song().signature_denominator
+            self._parent.log_message(self._scene.name + " is playing with " + str(numerator) + "/" + str(denominator))
+            self._parent.send_message('{SS|' + str(numerator) + '|' + str(denominator))
